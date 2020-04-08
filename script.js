ac5f88283f3a07381b10b94bc78329a5d12c1fc2
Dima <dima22kolis@ukr.net>
2020-04-08 23:21:57 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var a = 1;
+console.log(a);
+
+
+// var number = 5;
+// var string = "helo";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj  = {};
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone.name);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr[2]);
+
+
+// // alert("hello world")
+
+// let answer = prompt("are you 18 years old ?", "Yes");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+let isCHecked = false,
+    isClose = true;
+
+console.log(isCHecked && isClose);
